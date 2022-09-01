1470613b90979bcc8f879b54d4ee78373c22c1cd
Victor Caio <kaioaraujo426@gmail.com>
2022-09-01 14:37:52 -0300

    feat(auth): implement `@CurrentUser()`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
 import { IsPublic } from './auth/decorators/is-public.decorator';
+import { User } from './user/entities/user.entity';
 
 @Controller()
 export class AppController {
@@ -10,5 +12,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/me')
+  getMe(@CurrentUser() user: User) {
+    return user;
   }
 }
